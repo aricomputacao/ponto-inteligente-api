1fae97d053105e4dfd8ea0cb2f7245752aa42f90
José Ari <aricomputacao@gmail.com>
2018-06-22 00:13:04 -0300

    RODANDO TESTES 2

--- src/test/java/com/kazale/pontointeligente/api/repositories/LancamentoRepositoryTest.java
+++ src/test/java/com/kazale/pontointeligente/api/repositories/LancamentoRepositoryTest.java
@@ -65,6 +65,7 @@ public class LancamentoRepositoryTest {
 	}
 	@Test
 	public void testBuscarLancamentoPorFuncionarioIdPaginado() {
+		@SuppressWarnings("deprecation")
 		PageRequest page = new PageRequest(0,10);
 		Page<Lancamento> lancamentos = this.lancamentoRepository.findByFuncionarioId(funcionarioId,page);
 		
